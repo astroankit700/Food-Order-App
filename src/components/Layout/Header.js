2342f7205bad62b248b601a56b2261c1285b188d
astroankit700 <ankitagrawal700aa@gmail.com>
2022-03-07 19:37:28 +0530

    added ethnicity

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -5,17 +5,17 @@ import mealsImage from "../../assets/meals.jpg";
 import HeaderCartButton from "./HeaderCartButton";
 
 const Header = (props) => {
-  return (
-    <React.Fragment>
-      <header className={classes.header}>
-        <h1>ReactMeals</h1>
-        <HeaderCartButton onClick={props.onShowCart} />
-      </header>
-      <div className={classes["main-image"]}>
-        <img src={mealsImage} alt="A table full of delicious food" />
-      </div>
-    </React.Fragment>
-  );
+    return (
+        <React.Fragment>
+            <header className={classes.header}>
+                <h1>ReactBakery</h1>
+                <HeaderCartButton onClick={props.onShowCart} />
+            </header>
+            <div className={classes["main-image"]}>
+                <img src={mealsImage} alt="A table full of delicious food" />
+            </div>
+        </React.Fragment>
+    );
 };
 
 export default Header;
